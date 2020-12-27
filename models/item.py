27b399c2b15ff0bf9a13744b6925daf7bc1a9e18
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2020-12-27 15:56:02 +0000

    section 6 apicourse

--- models/item.py
+++ models/item.py
@@ -2,26 +2,29 @@ from db import db
 
 
 class ItemModel(db.Model):
-
     __tablename__ = 'items'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    def __init__(self, name, price):
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
+        self.store_id = store_id
 
     def json(self):
         return {'name': self.name, 'price': self.price}
 
     @classmethod
     def find_by_name(cls, name):
-        return ItemModel.query.filter_by(name=name).first() # SELECT * FROM items WHERE name=name LIMIT 1.
-        # This returns and ItemModel Object
+        return cls.query.filter_by(name=name).first()
 
     def save_to_db(self):
-        db.session.add(self) # SQLAlchemy stores Objects (ItemModel Object) into the db
+        db.session.add(self)
         db.session.commit()
 
     def delete_from_db(self):
